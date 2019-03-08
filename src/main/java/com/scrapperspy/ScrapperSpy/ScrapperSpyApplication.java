2254bbb419063bec274156d394a7dc6e5e93270d
nilesh.sodha <nilesh.sodha@gdn-commerce.com>
2019-03-08 20:41:19 +0530

    Initial Commmit

---
+++ src/main/java/com/scrapperspy/ScrapperSpy/ScrapperSpyApplication.java
@@ -0,0 +1,13 @@
+package com.scrapperspy.ScrapperSpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapperSpyApplication.class, args);
+	}
+
+}
